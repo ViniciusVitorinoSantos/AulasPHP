79e7ad78fcaf7871200e8f1b233bb2b628f3f673
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-03-23 07:44:41 -0300

    aulas PHP

---
+++ aula5/operadores.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página PHP</title>
+</head>
+<body>
+<?php
+echo "Olá, mundo!";
+?>
+</body>
+</html>
